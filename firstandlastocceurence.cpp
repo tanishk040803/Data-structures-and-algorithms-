a7c1342f180c54f0004f77e835a7f4e4d7b3d532
tanishk katiyar <tanishkkatiyar5@gmail.com>
2025-08-16 14:20:57 +0530

    Add files via upload

---
+++ firstandlastocceurence.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+using namespace std;
+int firstoccurence(int arr[],int size,int key){
+
+
+int start=0;
+int end=size-1;
+int mid=start+(end-start)/2;
+int ans=-1;
+
+while(start<=end){
+    if(arr[mid]==key){
+    ans=mid;
+    end=mid-1;
+    
+}
+
+if(key>arr[mid]){
+    start=mid+1;
+
+}
+
+else{
+    end=mid-1;
+}
+
+mid=start+(end-start)/2;
+}
+
+    return ans;
+
+
+
+}
+int lastoccurence(int arr[],int size,int key){
+
+
+int start=0;
+int end=size-1;
+int mid=start+(end-start)/2;
+int ans=-1;
+
+while(start<=end){
+    if(arr[mid]==key){
+    ans=mid;
+   start=mid+1;
+    
+}
+
+if(key>arr[mid]){
+start=mid+1;
+
+}
+
+else if(key<arr[mid]){
+    end=mid-1;
+}
+
+mid=start+(end-start)/2;
+}
+
+    return ans;
+
+
+
+}
+int main(){
+
+
+    int arr[9]={0,1,2,3,3,3,3,3,5};
+    int index1 = firstoccurence(arr,9,3);
+    int index2 = lastoccurence(arr,9,3);
+     cout<<"first and last occurence of key is :"<<index1<<" and "<<index2 <<endl;
+ 
+
+    
+    
+    return 0;
+}
+
+
+
+
